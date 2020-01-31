37360707d161a92653c16827f51a91aaa027314c
Anderson Montilla <montillaanderson@gmail.com>
2020-01-31 11:44:42 -0300

    Carga del formulario, de reclutamiento proyecto 23people

---
+++ src/main/java/com/example/prueba/controllers/ControllerBasic.java
@@ -0,0 +1,25 @@
+package com.example.prueba.controllers;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping("/prueba")
+
+public class ControllerBasic {
+
+	@GetMapping(path = { "/saludar", "/demo"})
+	public String greeting(@RequestParam (name="name", required=false, defaultValue="World") String name, Model model ){
+		 model.addAttribute("name", name);
+		
+
+		return "greeting";
+	}
+
+}
